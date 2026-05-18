87918178484e7f19d4c21a2c07eb8a6dda551a5b
SouzaX27 <g.souzacardoso27@gmail.com>
2026-05-18 09:05:58 -0300

    Novas Functions Adicionadas

--- script.js
+++ script.js
@@ -6,14 +6,49 @@ function mensagemBoasVindas(nome) {
   return `Olá, ${nome}!`;
 }
 
-function exibirMaior(a, b){
+function exibirMaior(a, b) {
   if (a>b){
     return a;
-  };
-  if (a<b){
+  } else {
     return b;
   }
 }
 
-module.exports = { somar, mensagemBoasVindas, exibirMaior };
+  // Implemente uma função que irá receber 2 valores. A função deverá exibir qual dos dois valores é par ou ímpar.
+
+  
+function parOuImpar(a, b) {
+  if (typeof a == Number && typeof b == Number) {
+    if (a%2 == 0 && b%2 == 0) {
+      return 'Ambos são pares'
+    }
+    if (a%2 == 0 && b%2 != 0) {
+      return 'Apenas A é par'
+    }
+    if (a%2 != 0 && b%2 != 0) {
+      return 'Ambos são ímpares'
+    }
+    if (a%2 != 0 && b%2 == 0) {
+      return 'Apenas B é par'
+    }
+  } else {
+    return 'Erro'
+  }
+};
+
+  // Crie uma função que receberá a idade e retornará se a pessoa pode votar , voto opcional ou não pode votar.
+
+  function podeVotar(a) {
+    if (a >= 16 && a < 18) {
+      return 'Voto Opcional'
+    }
+    if (a >= 18) {
+      return 'Voto Obrigatório'
+    }
+    else {
+      return 'Não Pode Votar'
+    }
+  }
+
+module.exports = { somar, mensagemBoasVindas, exibirMaior, parOuImpar, podeVotar };
 // export default { somar, mensagemBoasVindas };
--- script.test.js
+++ script.test.js
@@ -1,4 +1,4 @@
-const { somar, mensagemBoasVindas, exibirMaior } = require('./script');
+const { somar, mensagemBoasVindas, exibirMaior, parOuImpar, podeVotar } = require('./script');
 
 test('soma dois números', () => {
   expect(somar(2, 3)).toBe(5);
@@ -9,5 +9,21 @@ test('mensagem de boas vindas', () => {
 });
 
 test('maior número', () => {
-  expect(exibirMaior(2, 3)).toBe(3)
+  expect(exibirMaior(2, 3)).toBe(3);
+  expect(exibirMaior(9, 7)).toBe(9);
+  expect(exibirMaior(8, 8)).toBe(8);
+});
+
+test('par ou ímpar', () => {
+  expect(parOuImpar('a','b')).toBe('Erro');
+  expect(parOuImpar(2,3)).toBe('Apenas A é par');
+  expect(parOuImpar(2,3)).toBe('Apenas B é par');
+  expect(parOuImpar(2,3)).toBe('Ambos são pares');
+  expect(parOuImpar(2,3)).toBe('Ambos são ímpares');
+});
+
+test('Pode ou Deve Votar', () => {
+  expect(podeVotar(10)).toBe('Não Pode Votar');
+  expect(podeVotar(16)).toBe('Voto Opcional');
+  expect(podeVotar(22)).toBe('Voto Obrigatório');
 })
