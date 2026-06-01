adfd3b30d860c1ae8beade42a7319cb4256b76f6
SouzaX27 <g.souzacardoso27@gmail.com>
2026-06-01 15:04:13 -0300

    Atualização 01/06 - Lista Finalizada

--- script.js
+++ script.js
@@ -15,7 +15,6 @@ function exibirMaior(a, b) {
 }
 
   // Implemente uma função que irá receber 2 valores. A função deverá exibir qual dos dois valores é par ou ímpar.
-
   
 function parOuImpar(a, b) {
   if (typeof a !== 'number' || typeof b !== 'number') {
@@ -37,53 +36,136 @@ function parOuImpar(a, b) {
     
 };
 
-  function podeVotar(a) {
-    if (a >= 16 && a < 18) {
-      return 'Voto Opcional'
+function podeVotar(a) {
+  if (a >= 16 && a < 18) {
+    return 'Voto Opcional'
+  }
+  if (a >= 18) {
+    return 'Voto Obrigatório'
+  }
+  else {
+    return 'Não Pode Votar'
+  }
+}
+
+function positivoNegativoZero(a) {
+  if (typeof a !== 'number') {
+    return 'Erro'
+  } else {
+    if (a > 0) {
+      return 'A é positivo'
     }
-    if (a >= 18) {
-      return 'Voto Obrigatório'
+    if (a < 0) {
+      return 'A é negativo'
     }
     else {
-      return 'Não Pode Votar'
+      return 'A é igual a zero'
     }
   }
+}
 
-  function positivoNegativoZero(a) {
-    if (typeof a !== 'number') {
-      return 'Erro'
+function salarioBonus(a) {
+  if (typeof a !== 'number') {
+    return 'Erro'
+  } else {
+    if (a < 2000) {
+      return `Salário: R$${a.toFixed(2)} | Bonús: R$${(a*0.2).toFixed(2)}`
+    }
+    if (a >= 2000 && a <= 5000) {
+      return `Salário: R$${a.toFixed(2)} | Bonús: R$${(a*0.1).toFixed(2)}`
+    }
+    if (a > 5000) {
+      return `Salário: R$${a.toFixed(2)} | Bonús: R$${(a*0.05).toFixed(2)}`
+    }
+  }
+}
+
+function podeDirigir(a) {
+  if (typeof a !== 'number') {
+  return 'Erro'
+  } else {
+    if (a >= 18) {
+      return 'Pode Dirigir'
     } else {
-      if (a > 0) {
-        return 'A é positivo'
-      }
-      if (a < 0) {
-        return 'A é negativo'
-      }
-      else {
-        return 'A é igual a zero'
-      }
+      return 'Não Pode Dirigir'
     }
   }
+}
 
-  // - Crie uma função que irá receber o salário e retorne o bônus.
-  //   - 20% se salário é < 2000
-  //   - 10% se salário entre 2000 e 5000.
-  //   - 5% salário acima de 5000.
+function exibirSemestre(mes) {
+  if (typeof a == 'number') {
+    return 'Erro'
+  } else {
+    switch (mes.toLowerCase()) {
+      case 'janeiro':
+      case 'fevereiro':
+      case 'março':
+      case 'abril':
+      case 'maio':
+      case 'junho':
+        return 'Primeiro Semestre';
+        break;
+  
+      case 'julho':
+      case 'agosto':
+      case 'setembro':
+      case 'outubro':
+      case 'novembro':
+      case 'dezembro':
+        return 'Segundo Semestre';
+        break;
+    }
+  }
+}
 
-  function salarioBonus(a) {
-    if (typeof a !== 'number') {
-      return 'Erro'
+function vogalOuConsoante (a) {
+  if (typeof a == 'number') {
+    return 'Erro'
+  } else {
+    switch (a.toLowerCase()) {
+      case 'a':
+      case 'e':
+      case 'i':
+      case 'o':
+      case 'u':
+        return 'Vogal'
+        break;
+    
+      default:
+        return 'Consoante'
+        break;
+    }
+  }
+}
+
+function exibirMedia(array) {
+  let media = 0;
+  for (i = 0; i < array.length; i++) {
+    let valor = array[i];
+    if (typeof valor !== 'number') {
+      return 'Erro'  
     } else {
-      if (a < 2000) {
-        return `Salário: R$${a.toFixed(2)} | Bonús: R$${(a*0.2).toFixed(2)}`
-      }
-      if (a >= 2000 && a <= 5000) {
-        return `Salário: R$${a.toFixed(2)} | Bonús: R$${(a*0.1).toFixed(2)}`
-      }
-      if (a > 5000) {
-        return `Salário: R$${a.toFixed(2)} | Bonús: R$${(a*0.05).toFixed(2)}`
-      }
+      media += (valor/5);
     }
   }
+  if (i !== 5) {
+    return 'Erro'
+  } else {
+    return media
+  }
+}
 
-module.exports = { somar, mensagemBoasVindas, exibirMaior, parOuImpar, podeVotar, positivoNegativoZero, salarioBonus };
+function fazerLogin(usuario, senha) {
+  if (usuario === 'Admin' && senha === '123') {
+    return 'Conectado com Sucesso!'
+  } else {
+    return 'Acesso negado! Usuário ou senha incorretos!'
+  }
+}
+
+
+
+
+module.exports = { somar, mensagemBoasVindas, exibirMaior, parOuImpar, podeVotar, positivoNegativoZero, salarioBonus, podeDirigir,
+  exibirSemestre, vogalOuConsoante, exibirMedia, fazerLogin
+ };
--- script.test.js
+++ script.test.js
@@ -1,4 +1,6 @@
-const { somar, mensagemBoasVindas, exibirMaior, parOuImpar, podeVotar, positivoNegativoZero, salarioBonus } = require('./script');
+const { somar, mensagemBoasVindas, exibirMaior, parOuImpar, podeVotar, positivoNegativoZero, salarioBonus, podeDirigir, exibirSemestre,
+  vogalOuConsoante, exibirMedia, fazerLogin
+ } = require('./script');
 
 test('soma dois números', () => {
   expect(somar(2, 3)).toBe(5);
@@ -40,4 +42,33 @@ test('Bonús do Salário', () => {
   expect(salarioBonus('1500')).toBe('Salário: R$1500.00 | Bonús: R$300.00');
   expect(salarioBonus('2500')).toBe('Salário: R$2500.00 | Bonús: R$250.00');
   expect(salarioBonus('5750')).toBe('Salário: R$5750.00 | Bonús: R$287.50');
+})
+
+test('Pode ou Não Pode Dirigir', () => {
+  expect(podeDirigir('a')).toBe('Erro');
+  expect(podeDirigir('19')).toBe('Pode Dirigir');
+  expect(podeDirigir('16')).toBe('Não Pode Dirigir');
+})
+
+test('Primeiro ou Segundo Semestre', () => {
+  expect(exibirSemestre(2)).toBe('Erro');
+  expect(exibirSemestre('fevereiro')).toBe('Primeiro Semestre');
+  expect(exibirSemestre('novembro')).toBe('Segundo Semestre');
+})
+
+test('Vogal ou Consoante', () => {
+  expect(vogalOuConsoante(2)).toBe('Erro');
+  expect(vogalOuConsoante('A')).toBe('Vogal');
+  expect(vogalOuConsoante('Z')).toBe('Consoante');
+})
+
+test('Média de 5 valores', () => {
+  expect(exibirMedia(['a', 5, 3, 1, 3])).toBe('Erro');
+  expect(exibirMedia([1, 5, 3, 1])).toBe('Erro');
+  expect(exibirMedia([10, 10, 10, 10, 10])).toBe(10);
+})
+
+test('Login de Admin', () => {
+  expect(fazerLogin('Ambrozina','321')).toBe('Acesso negado! Usuário ou senha incorretos!');
+  expect(fazerLogin('Admin','123')).toBe('Conectado com Sucesso!');
 })
